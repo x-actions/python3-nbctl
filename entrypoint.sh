0277a8ca36ea99b3ed7d972e48e56477d69accd0
xiexianbin <me@xiexianbin.cn>
2023-06-24 16:08:49 +0800

    func: convert ipynb to markdown
    Signed-off-by: xiexianbin <me@xiexianbin.cn>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,15 @@
 set -e
 
 export LOG_LEVEL=${LOG_LEVEL:-"DEBUG"}
-export SCRIPT=${SCRIPT:-"`date`"}
+export SCRIPT=${SCRIPT:-"echo `date`"}
 
 echo $(pwd)
 echo "## Check Package Version ##################"
 bash --version
 git version
+
+echo "env is:"
+env
 
 echo "## Run nbctl ##################"
 
